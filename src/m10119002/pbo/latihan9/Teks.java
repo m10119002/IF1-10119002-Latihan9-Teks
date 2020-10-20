3cc1ce35a17dd8464f0c499fe943d27dc1527e98
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:45:49 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan9/Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan9;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : IF-1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program teks
+ * class
+ * 
+ */
+public class Teks {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+}
